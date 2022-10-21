c30d6be7775eaad4b2a42c50519c32f497d7e16e
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-10-21 14:53:31 -0700

    start of homepage functionality

---
+++ src/components/Homepage/index.jsx
@@ -0,0 +1,48 @@
+const Homepage = ({ setStart, setDataBreakpoint }) => {
+  const handleSelection = (start, end) => {
+    setStart(true);
+    setDataBreakpoint(() => {
+      return { start: start, end: end };
+    });
+  };
+  const randomStartNum = () => {
+    Math.floor(Math.random() * 99);
+  };
+
+  return (
+    <ul>
+      {/* questions 1-57 */}
+      <li>
+        <button onClick={() => handleSelection(0, 56)}>
+          Category 1: AMERICAN GOVERNMENT
+        </button>
+      </li>
+      {/* questions 58-87 */}
+      <li>
+        <button onClick={() => handleSelection(57, 86)}>
+          Category 2: AMERICAN HISTORY
+        </button>
+      </li>
+      {/* questions: 88-100 */}
+      <li>
+        <button onClick={() => handleSelection(87, 99)}>
+          Category 3: OTHER
+        </button>
+      </li>
+      <li>
+        <button onClick={() => handleSelection(0, 99)}>All</button>
+      </li>
+      <li>
+        {/* TODO: randomize 10 questions */}
+        <button onClick={() => handleSelection(randomStartNum, 10)}>
+          Practice Test
+        </button>
+      </li>
+      <li>
+        <button>Bookmark</button>
+      </li>
+    </ul>
+  );
+};
+
+export default Homepage;
