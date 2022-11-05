e4aa1a2087fba569cbde7dd7587c4f5db3893373
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-11-04 17:36:03 -0700

    home screen layout

--- src/components/Homepage/index.jsx
+++ src/components/Homepage/index.jsx
@@ -1,31 +1,43 @@
 import { Link } from "react-router-dom";
+import styles from "./styles.module.scss";
 
 const Homepage = () => {
   return (
-    <ul>
-      {/* questions 1-57 */}
-      <li>
-        <Link to="/category/1"> Category 1: AMERICAN GOVERNMENT</Link>
-      </li>
-      {/* questions 58-87 */}
-      <li>
-        <Link to="/category/2"> Category 2: AMERICAN HISTORY</Link>
-      </li>
-      {/* questions: 88-100 */}
-      <li>
-        <Link to="/category/3"> Category 3: OTHER</Link>
-      </li>
-      <li>
-        <Link to="/category/all">All</Link>
-      </li>
-      {/* <li> */}
-      {/* TODO: randomize 10 questions */}
-      {/* Practice Test
+    <>
+      <h1 className={styles.title}>Civics Test: 2008 Version</h1>
+      <ul className={styles.linkContainer}>
+        {/* questions 1-57 */}
+        <li className={styles.linkWrap}>
+          <Link to="/category/1" className={styles.link}>
+            Category 1: AMERICAN GOVERNMENT
+          </Link>
+        </li>
+        {/* questions 58-87 */}
+        <li className={styles.linkWrap}>
+          <Link to="/category/2" className={styles.link}>
+            Category 2: AMERICAN HISTORY
+          </Link>
+        </li>
+        {/* questions: 88-100 */}
+        <li className={styles.linkWrap}>
+          <Link to="/category/3" className={styles.link}>
+            Category 3: OTHER
+          </Link>
+        </li>
+        <li className={styles.linkWrap}>
+          <Link to="/category/all" className={styles.link}>
+            All
+          </Link>
+        </li>
+        {/* <li> */}
+        {/* TODO: randomize 10 questions */}
+        {/* Practice Test
       </li>
       <li>
         <Link to="/bookmark">Bookmark</Link>
       </li> */}
-    </ul>
+      </ul>
+    </>
   );
 };
 
