89f2b7ab5922c79d84e875043c2fbfec45ee8b4e
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-11-11 08:55:29 -0800

    object destructoring to fix bookmark bug

--- src/components/CardLayout/index.jsx
+++ src/components/CardLayout/index.jsx
@@ -4,20 +4,19 @@ import { useDispatch, useSelector } from "react-redux";
 import { add, remove } from "../../store/bookmarksSlice";
 import { useState } from "react";
 const CardLayout = ({ data, index, totalQ, testMode }) => {
-  const { question, options, cardId } = data;
+  const { question, options, id } = data;
   const [activeFront, setActiveFront] = useState(true);
-
   // get state of bookmarks
   const bookmarkState = useSelector((state) => state.bookmarks);
   const dispatch = useDispatch();
   const handleBookmark = () => {
-    dispatch(add({ index: cardId }));
+    dispatch(add({ index: id }));
   };
   const handleUnbookmark = () => {
-    dispatch(remove({ index: cardId }));
+    dispatch(remove({ index: id }));
   };
   // return boolean on if individual card is bookmarked
-  const isBookmarked = bookmarkState.indexOf(cardId) >= 0;
+  const isBookmarked = bookmarkState.indexOf(id) >= 0;
 
   return (
     <div className={styles.outer}>
