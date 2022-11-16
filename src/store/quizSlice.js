255c6a9761906da32591915f908eb796d39c3a85
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-11-16 15:53:58 -0800

    reducer for correct and incorrect quiz answers

--- src/store/quizSlice.js
+++ src/store/quizSlice.js
@@ -4,12 +4,25 @@ export const quizSlice = createSlice({
   name: "quiz",
   initialState: {
     ids: [],
+    stats: {},
   },
   reducers: {
     start: (state, action) => {
       return { ...state, ids: action.payload.indexes };
     },
+    correct: (state, action) => {
+      state.stats = {
+        ...state.stats,
+        [action.payload.id]: action.payload.answer,
+      };
+    },
+    incorrect: (state, action) => {
+      state.stats = {
+        ...state.stats,
+        [action.payload.id]: action.payload.answer,
+      };
+    },
   },
 });
-export const { start } = quizSlice.actions;
+export const { start, correct, incorrect } = quizSlice.actions;
 export default quizSlice.reducer;
