078b90d1c59912cc2fd3a0ccee0b54926cba33de
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-11-17 16:57:32 -0800

    minor answer changes to data

--- src/assets/grouped.js
+++ src/assets/grouped.js
@@ -1,10 +1,26 @@
 import data from "./output.json";
 
 const groups = {
-  1: { title: "American Government", data: data.questions.slice(0, 57) },
-  2: { title: "American History", data: data.questions.slice(57, 87) },
-  3: { title: "Other", data: data.questions.slice(87, 100) },
-  all: { title: "All", data: data.questions },
+  1: {
+    title: "American Government",
+    data: data.questions.slice(0, 57),
+    minNum: 0,
+    maxNum: 57,
+  },
+  2: {
+    title: "American History",
+    data: data.questions.slice(57, 87),
+    minNum: 58,
+    maxNum: 87,
+  },
+
+  3: {
+    title: "Other",
+    data: data.questions.slice(87, 100),
+    minNum: 88,
+    maxNum: 100,
+  },
+  all: { title: "All", data: data.questions, minNum: 1, maxNum: 100 },
 };
 
 export default groups;
