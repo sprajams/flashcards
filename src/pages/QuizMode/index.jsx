c0a04f552cee0d32e3e6353a5d5dae77f9e6cbe3
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-11-18 16:34:51 -0800

    quiz mode to handle different categories

--- src/pages/QuizMode/index.jsx
+++ src/pages/QuizMode/index.jsx
@@ -1,15 +1,18 @@
 import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { start } from "../../store/quizSlice";
 import data from "../../assets/output.json";
+import groups from "../../assets/grouped";
 import CardLayout from "../../components/CardLayout";
 import Results from "../Results";
 
 const QuizMode = () => {
+  const { categoryId } = useParams();
   const [activeIndex, setActiveIndex] = useState(0);
   const dispatch = useDispatch();
   const quiz = useSelector((state) => state.quiz);
-  // create array of 10 random non-repeating indexes to use for quiz ?s
+
   const handleSkip = () => {
     if (activeIndex < 10) {
       setActiveIndex(() => {
@@ -18,18 +21,31 @@ const QuizMode = () => {
     }
   };
 
+  // create array of 10 random non-repeating indexes to use for quiz ?s
   useEffect(() => {
     let result = [];
     while (result.length < 10) {
       let randomNum = Math.floor(Math.random() * 100);
+      if (groups[categoryId]) {
+        randomNum = Math.floor(
+          Math.random() *
+            (groups[categoryId].maxNum - groups[categoryId].minNum + 1) +
+            groups[categoryId].minNum
+        );
+      }
+      // weed out duplicates
       if (!result.includes(randomNum)) {
         result.push(randomNum);
       }
     }
-    result.sort((a, b) => a - b);
     dispatch(start({ indexes: result }));
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const quizTitle = groups[categoryId]
+    ? "Quiz: " + groups[categoryId].title
+    : "Quiz";
+
   return (
     <>
       {quiz.ids.length > 0 && activeIndex !== 10 ? (
@@ -37,13 +53,13 @@ const QuizMode = () => {
           data={data.questions.find(({ id }) => quiz.ids[activeIndex] === id)}
           index={activeIndex}
           totalQ={quiz.ids.length}
-          title={"Quiz"}
+          title={quizTitle}
           handleSkip={handleSkip}
           isQuiz={true}
           quizId={quiz.ids[activeIndex]}
         />
       ) : activeIndex === 10 ? (
-        <Results />
+        <Results title={quizTitle} />
       ) : null}
     </>
   );
