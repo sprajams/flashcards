42ba8cd240cf2b51bcbd6092961d7cb3cdfaef22
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-11-18 16:35:35 -0800

    functionality of retry quiz button

--- src/pages/Results/index.jsx
+++ src/pages/Results/index.jsx
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-const Results = () => {
+const Results = ({ title }) => {
   const quiz = useSelector((state) => state.quiz);
   // turn stats object into [[key,value], [id, true]]
   const statsEntries = Object.entries(quiz.stats);
@@ -20,16 +21,22 @@ const Results = () => {
   );
 
   const numCorrect = results.correct.length;
-
   const resultStatus = numCorrect >= 6;
+
+  const navigate = useNavigate();
+
+  const handleRetry = () => {
+    navigate(0); // refresh page to start new quiz
+  };
 
   return (
     <div>
+      <h3>{title}</h3>
       <h2>Final Score</h2>
       <div>
         {resultStatus ? <h3>YAY</h3> : <h3>oh no</h3>}
         <h4>{numCorrect} / 10</h4>
-        <button>retry</button>
+        <button onClick={handleRetry}>retry</button>
       </div>
     </div>
   );
