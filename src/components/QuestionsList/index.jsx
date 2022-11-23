2733f6af2fe34ca9e0008ffa5e7e1059f978daef
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-11-22 19:11:55 -0800

    results correct/incorrect icons

--- src/components/QuestionsList/index.jsx
+++ src/components/QuestionsList/index.jsx
@@ -1,14 +1,32 @@
 import { Link } from "react-router-dom";
+import { HiCheck, HiX } from "react-icons/hi";
 import styles from "./styles.module.scss";
+import clsx from "clsx";
 
-const QuestionsList = ({ data }) => {
+const QuestionsList = ({ data, results }) => {
   return (
     <ul className={styles.listWrap}>
       {data.map((x, i) => {
+        const correct = results
+          ? results.correct.includes(x.id.toString())
+          : null;
         return (
           <li key={i} className={styles.questionWrap}>
             <Link to={`${i + 1}`} className={styles.questionLink}>
-              {x.question}
+              <span>{x.question}</span>
+              {results ? (
+                correct ? (
+                  <span className={styles.iconWrap}>
+                    <HiCheck
+                      className={clsx(styles.icon, styles.correctIcon)}
+                    />
+                  </span>
+                ) : (
+                  <span className={styles.iconWrap}>
+                    <HiX className={clsx(styles.icon, styles.incorrectIcon)} />
+                  </span>
+                )
+              ) : null}
             </Link>
           </li>
         );
