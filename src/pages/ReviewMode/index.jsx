64c9f2310e377a45ae040cb49c51fcb218fd4bdd
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-11-30 15:42:36 -0800

    review mode component

---
+++ src/pages/ReviewMode/index.jsx
@@ -0,0 +1,23 @@
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import CardLayout from "../../components/CardLayout";
+
+const ReviewMode = () => {
+  const quiz = useSelector((state) => state.quiz);
+  const { cardIndex } = useParams();
+  let activeIndex = parseInt(cardIndex);
+  console.log(quiz.data[activeIndex]);
+  return (
+    <>
+      <CardLayout
+        title={"Quiz Review"}
+        data={quiz.data[activeIndex - 1]}
+        isReview={true}
+        activeIndex={activeIndex}
+        totalQ={quiz.data.length}
+      />
+    </>
+  );
+};
+
+export default ReviewMode;
